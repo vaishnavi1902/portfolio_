d3bbb1eaaebdcf21488d4e66d1a213e138c2db18
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-09-12 20:48:00 +0530

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
 import './about.css'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderActive} from 'react-icons/vsc'
+import {GiSkills} from 'react-icons/gi'
+import ME from '../../assets/me2.jpg'
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me !</h2>
+      <div className='container about__container'>
+        <div className="about__me">
+            <div className="about__me-img">
+              <img src={ME} alt="" />
+            </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>1+ Years experiance in SD</small>
+            </article>
+            <article className="about__card">
+              <GiSkills className='about__icon' />
+              <h5>Skills</h5>
+              <small>Skilled in Technical Practices</small>
+            </article>
+            <article className="about__card">
+              <VscFolderActive className='about__icon' />
+              <h5>Projects</h5>
+              <small>15+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+          Hello World ! I am a professional Software Engineer and 
+          having 1+ years of experience in Website development. I am familiar with 
+          Java and a good intermediate level of Data Structures and Algorithms . 
+          Also having a 6 months+ experience in ReactJS and React Native development .
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
@@ -6,13 +7,13 @@ const HeaderSocials = () => {
   return (
     <>
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/vaishnavi-kumbhakarna-838a13221/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/vaishnavi1902" target='_blank'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/vaishnavi-kumbhakarna-838a13221/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/vaishnavi1902" target='_blank' rel="noreferrer"><BsGithub/></a>
         <a href="#"><BsGlobe/></a>
     </div>
      <div className='header__socials_sm'>
-        <a href="https://www.linkedin.com/in/vaishnavi-kumbhakarna-838a13221/" target='_blank'><BsLinkedin/></a>
-        <a href="https://github.com/vaishnavi1902" target='_blank'><BsGithub/></a>
+        <a href="https://www.linkedin.com/in/vaishnavi-kumbhakarna-838a13221/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/vaishnavi1902" target='_blank' rel="noreferrer"><BsGithub/></a>
         <a href="#"><BsGlobe/></a>
      </div>
  </>
