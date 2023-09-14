785633f29c132f1b9dcb06ba3c148fb81270acb8
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-09-14 14:07:33 +0530

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {AiOutlineMail} from 'react-icons/ai'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className=''>
+      <ul className='all_sections'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="socials">
+        <a href="https://www.linkedin.com/in/vaishnavi-kumbhakarna-838a13221/" className='icons'><BsLinkedin /></a>
+        <a href="http://github.com/vaishnavi1902" className='icons'><BsGithub /></a>
+        <a href="mailto:kumbhakarna.vaishnavi01@gmail.com" className='icons'><AiOutlineMail /></a>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from  '../../assets/Vaishnavi_kumbhakarna_Resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a className='btn' href={CV} download>Download CV</a>
+        {/* <a className='btn' href={CV} download>Download CV</a> */}
         <a className='btn btn-primary' href="#contact">Let's TalK </a>
     </div>
   )
