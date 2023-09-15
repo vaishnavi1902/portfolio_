0a7c8d3e2606ec84c40b193c0ce29ef2970b5a72
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-09-15 15:46:03 +0530

    portfolio

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
-const Project = () => {
-  return (
-    <div>Project</div>
+import './projects.css'
+import data from './projects.json'
+const Project = function Project () {
+  const ddata = data.map(
+    (item) => {
+      return (
+        <div>
+            <h4>{item.name}</h4>
+            <h5>{item.github}</h5>
+        </div>
+      )
+    }
   )
+  return ddata;
 }
-
 export default Project
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
+import Project from './Project'
 import './projects.css'
 const Projects = () => {
   return (
-    <section id='projects' className=' container  projects__container'>
-       <h5>My recent</h5>
+    <section id='projects' className='container projects__container'>
+      <h5>My recent</h5>
       <h2>Projects !</h2>
-      <div className="">
-         <div className="project__container">
-          Project 1
-         </div>
-      </div>
+      <Project/>
     </section>
   )
 }
