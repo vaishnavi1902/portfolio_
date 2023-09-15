2a870877ebdf960f0d9f1f42b268daaa4bf97420
vaishnavi1902 <vaishnavikumbhakarna467@gmail.com>
2023-09-15 16:00:59 +0530

    portfolio

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import './projects.css'
-import data from './projects.json'
-const Project = function Project () {
-  const ddata = data.map(
-    (item) => {
-      return (
-        <div>
-            <h4>{item.name}</h4>
-            <h5>{item.github}</h5>
-        </div>
-      )
-    }
+
+const Project = () => {
+  return (
+    <div>Project</div>
   )
-  return ddata;
-}
-export default Project
+} 
+
+export default Project 
